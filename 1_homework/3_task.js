c1861eca8ff67926579e291b55616accef4fb441
Ringo <Nickos_K@mail.ru>
2020-01-24 10:58:50 +0300

    Task 3 created

---
+++ 1_homework/3_task.js
@@ -0,0 +1,94 @@
+let countries = [
+    {
+        id: 1,
+        country: "Belarus"
+    },
+    {
+        id: 2,
+        country: "Russia"
+    },
+    {
+        id: 3,
+        country: "Ukraine"
+    }
+]
+
+let capitals = [
+    {
+        id: 1,
+        name: 'Moscow',
+        countryId: 2
+    },
+    {
+        id: 2,
+        name: 'Kiev',
+        countryId: 3
+    },
+    {
+        id: 3,
+        name: 'Minsk',
+        countryId: 1
+    },
+]
+
+function loadCountryById(id) {
+    return new Promise(((resolve, reject) => {
+        if (Math.random() < 0.05) {
+            console.log('Could not retrieve the data')
+        } else {
+            countries.forEach(element => {
+                if (element.id == id) {
+                    setTimeout(resolve(element), 3000)
+                }
+            })
+            reject(new Error('Country not found'))
+        }
+    }))
+}
+
+function loadCountry({id, name}) {
+    return new Promise((resolve, reject) => {
+            if (id) {
+                countries.forEach(element => {
+                    if (element.id == id) {
+                        resolve(element)
+                    }
+                })
+                reject(new Error('Country not found'))
+            } else if (name) {
+                countries.forEach(element => {
+                    if (element.country == name) {
+                        setTimeout(resolve(element), 3000)
+                    }
+                })
+                reject(new Error('Country not found'))
+            }
+        }
+    )
+}
+
+function loadCapitalByCountryId(id) {
+    return new Promise(((resolve, reject) => {
+        capitals.forEach(element => {
+            if (element.countryId == id) {
+                resolve(element.name)
+            }
+        })
+        reject(new Error('Country not found'))
+
+    }))
+}
+
+function getCapitalByCountry({countryId, countryName}){
+    return  loadCountry({id: countryId, name: countryName}).then(country=> {
+        return console.log(loadCapitalByCountryId(country.id))
+    })
+}
+
+// console.log(loadCountryById(2))
+//
+// console.log(loadCountry({id: 2}))
+//
+// console.log(loadCapitalByCountryId(2))
+
+console.log(getCapitalByCountry({countryId: 1}))
