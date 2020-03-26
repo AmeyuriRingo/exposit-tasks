6544fc37bb245f9a586e41c2c4c6944e977db122
AmeyuriRingo <nickos_k@mail.ru>
2020-03-26 16:46:39 +0300

    bugfix

--- react_homework/src/redux/createStore.js
+++ react_homework/src/redux/createStore.js
@@ -5,8 +5,7 @@ import thunk from "redux-thunk";
 const store = createStore(rootReducer, initialState, compose(
     applyMiddleware(
         thunk
-    ),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 ))
 
 export default store
